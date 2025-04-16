893d6a716a7989e28841044cd71783fc282a2193
git-spec <inkyfischer@gmail.com>
2025-04-16 21:57:18 +0200

    feat: Set name of record Character for collection in MongoDB.

--- src/main/java/com/example/spring_data/model/Character.java
+++ src/main/java/com/example/spring_data/model/Character.java
@@ -1,5 +1,9 @@
 package com.example.spring_data.model;
 
+import org.springframework.data.mongodb.core.mapping.Document;
+
+
+@Document("Characters")
 public record Character(String id, String name, int age, String type) {
 
 }
