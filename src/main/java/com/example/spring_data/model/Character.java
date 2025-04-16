61201b85cbd83666d50df8b4b6bc96501ac91a65
git-spec <inkyfischer@gmail.com>
2025-04-16 13:26:08 +0200

    refactor: Change record Student to Character.

---
+++ src/main/java/com/example/spring_data/model/Character.java
@@ -0,0 +1,5 @@
+package com.example.spring_data.model;
+
+public record Character(String id, String name, int age, String type) {
+
+}
--- src/main/java/com/example/spring_data/model/Student.java
+++
@@ -1,5 +0,0 @@
-package com.example.spring_data.model;
-
-public record Student(String id, String name, int age) {
-
-}
