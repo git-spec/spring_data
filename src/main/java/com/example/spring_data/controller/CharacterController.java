028bf707309cd2361fa2c768c32b3c91ce106b83
git-spec <inkyfischer@gmail.com>
2025-04-16 22:03:24 +0200

    feat: Combine CharacterRepo with methods in CharacterController.

--- src/main/java/com/example/spring_data/controller/CharacterController.java
+++ src/main/java/com/example/spring_data/controller/CharacterController.java
@@ -24,27 +24,28 @@ public class CharacterController {
 
     @GetMapping
     public List<Character> getAllCharacters() {
-        return List.of(
-            new Character("1", "Asterix", 35, "Warrior"),
-            new Character("2", "Obelix", 35, "Supplier"),
-            new Character("3", "Miraculix", 60, "Druid"),
-            new Character("4", "Majestix", 60, "Chief"),
-            new Character("5", "Troubadix", 25, "Bard"),
-            new Character("6", "Gutemine", 35, "Chiefs Wife"),
-            new Character("7", "Idefix", 5, "Dog"),
-            new Character("8", "Geriatrix", 70, "Retiree"),
-            new Character("9", "Automatix", 35, "Smith"),
-            new Character("10", "Grockelix", 35, "Fisherman")
-        );
+        // return List.of(
+        //     new Character("1", "Asterix", 35, "Warrior"),
+        //     new Character("2", "Obelix", 35, "Supplier"),
+        //     new Character("3", "Miraculix", 60, "Druid"),
+        //     new Character("4", "Majestix", 60, "Chief"),
+        //     new Character("5", "Troubadix", 25, "Bard"),
+        //     new Character("6", "Gutemine", 35, "Chiefs Wife"),
+        //     new Character("7", "Idefix", 5, "Dog"),
+        //     new Character("8", "Geriatrix", 70, "Retiree"),
+        //     new Character("9", "Automatix", 35, "Smith"),
+        //     new Character("10", "Grockelix", 35, "Fisherman")
+        // );
+        return repo.findAll();
     }
 
     @GetMapping("/{id}")
     public Character getCharacterByID(@PathVariable String id) {
-        return null;
+        return repo.findById(id).orElseThrow(null);
     }
 
     @PostMapping
     public Character addCharacter(@RequestBody Character student) {
-        return null;
+        return repo.save(student);
     }
 }
