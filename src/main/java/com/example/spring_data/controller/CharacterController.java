5861ea05afcf9514b67793dade3c63b61c0312b5
git-spec <inkyfischer@gmail.com>
2025-04-17 08:38:51 +0200

    feat: Create method deleteCharacterByID in CharacterController.

--- src/main/java/com/example/spring_data/controller/CharacterController.java
+++ src/main/java/com/example/spring_data/controller/CharacterController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -47,5 +48,10 @@ public class CharacterController {
     @PostMapping
     public Character addCharacter(@RequestBody Character student) {
         return repo.save(student);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteCharacterByID(@PathVariable String id) {
+        repo.deleteById(id);
     }
 }
