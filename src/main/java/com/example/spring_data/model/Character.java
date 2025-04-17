0e4e86e72759d20a8241470aa2d200524fc5df67
git-spec <inkyfischer@gmail.com>
2025-04-17 10:23:26 +0200

    feat: Create CharacterDTO.

--- src/main/java/com/example/spring_data/model/Character.java
+++ src/main/java/com/example/spring_data/model/Character.java
@@ -4,6 +4,4 @@ import org.springframework.data.mongodb.core.mapping.Document;
 
 
 @Document("Characters")
-public record Character(String id, String name, int age, String type) {
-
-}
+public record Character(String id, String name, int age, String profession) {}
