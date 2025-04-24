1568eb9c495c7cc8f4682e19bc61463a6c86a6af
git-spec <inkyfischer@gmail.com>
2025-04-24 21:03:06 +0200

    fix: Add annotation Service to IDService.

--- src/main/java/com/example/spring_data/service/IDService.java
+++ src/main/java/com/example/spring_data/service/IDService.java
@@ -2,7 +2,10 @@ package com.example.spring_data.service;
 
 import java.util.UUID;
 
+import org.springframework.stereotype.Service;
 
+
+@Service
 public class IDService {
     public String createID() {
         return UUID.randomUUID().toString();
